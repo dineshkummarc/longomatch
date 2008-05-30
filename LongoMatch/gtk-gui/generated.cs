4a189c92e35530cdd52f3e5976767a980daeec4a
longomatch <longomatch@f36f3c29-b420-0410-aad8-acd80f50a564>
2008-05-30 20:26:10 +0000

    Corrected compilation problems on CsharpDevelop

--- LongoMatch/gtk-gui/generated.cs
+++ LongoMatch/gtk-gui/generated.cs
@@ -18,43 +18,6 @@ namespace Stetic {
         internal static void Initialize(Gtk.Widget iconRenderer) {
             if ((Stetic.Gui.initialized == false)) {
                 Stetic.Gui.initialized = true;
-                Gtk.IconFactory w1 = new Gtk.IconFactory();
-                Gtk.IconSet w2 = new Gtk.IconSet(Stetic.IconLoader.LoadIcon(iconRenderer, "gtk-color-picker", Gtk.IconSize.Menu, 16));
-                w1.Add("ygg", w2);
-                w1.AddDefault();
-            }
-        }
-    }
-    
-    internal class IconLoader {
-        
-        public static Gdk.Pixbuf LoadIcon(Gtk.Widget widget, string name, Gtk.IconSize size, int sz) {
-            Gdk.Pixbuf res = widget.RenderIcon(name, size, null);
-            if ((res != null)) {
-                return res;
-            }
-            else {
-                try {
-                    return Gtk.IconTheme.Default.LoadIcon(name, sz, 0);
-                }
-                catch (System.Exception ) {
-                    if ((name != "gtk-missing-image")) {
-                        return Stetic.IconLoader.LoadIcon(widget, "gtk-missing-image", size, sz);
-                    }
-                    else {
-                        Gdk.Pixmap pmap = new Gdk.Pixmap(Gdk.Screen.Default.RootWindow, sz, sz);
-                        Gdk.GC gc = new Gdk.GC(pmap);
-                        gc.RgbFgColor = new Gdk.Color(255, 255, 255);
-                        pmap.DrawRectangle(gc, true, 0, 0, sz, sz);
-                        gc.RgbFgColor = new Gdk.Color(0, 0, 0);
-                        pmap.DrawRectangle(gc, false, 0, 0, (sz - 1), (sz - 1));
-                        gc.SetLineAttributes(3, Gdk.LineStyle.Solid, Gdk.CapStyle.Round, Gdk.JoinStyle.Round);
-                        gc.RgbFgColor = new Gdk.Color(255, 0, 0);
-                        pmap.DrawLine(gc, (sz / 4), (sz / 4), ((sz - 1) - (sz / 4)), ((sz - 1) - (sz / 4)));
-                        pmap.DrawLine(gc, ((sz - 1) - (sz / 4)), (sz / 4), (sz / 4), ((sz - 1) - (sz / 4)));
-                        return Gdk.Pixbuf.FromDrawable(pmap, pmap.Colormap, 0, 0, 0, 0, sz, sz);
-                    }
-                }
             }
         }
     }
@@ -101,6 +64,39 @@ namespace Stetic {
                 if (((w != null) && typeof(Gtk.Window).IsInstanceOfType(w))) {
                     ((Gtk.Window)(w)).AddAccelGroup(this.uimanager.AccelGroup);
                     this.uimanager = null;
+                }
+            }
+        }
+    }
+    
+    internal class IconLoader {
+        
+        public static Gdk.Pixbuf LoadIcon(Gtk.Widget widget, string name, Gtk.IconSize size, int sz) {
+            Gdk.Pixbuf res = widget.RenderIcon(name, size, null);
+            if ((res != null)) {
+                return res;
+            }
+            else {
+                try {
+                    return Gtk.IconTheme.Default.LoadIcon(name, sz, 0);
+                }
+                catch (System.Exception ) {
+                    if ((name != "gtk-missing-image")) {
+                        return Stetic.IconLoader.LoadIcon(widget, "gtk-missing-image", size, sz);
+                    }
+                    else {
+                        Gdk.Pixmap pmap = new Gdk.Pixmap(Gdk.Screen.Default.RootWindow, sz, sz);
+                        Gdk.GC gc = new Gdk.GC(pmap);
+                        gc.RgbFgColor = new Gdk.Color(255, 255, 255);
+                        pmap.DrawRectangle(gc, true, 0, 0, sz, sz);
+                        gc.RgbFgColor = new Gdk.Color(0, 0, 0);
+                        pmap.DrawRectangle(gc, false, 0, 0, (sz - 1), (sz - 1));
+                        gc.SetLineAttributes(3, Gdk.LineStyle.Solid, Gdk.CapStyle.Round, Gdk.JoinStyle.Round);
+                        gc.RgbFgColor = new Gdk.Color(255, 0, 0);
+                        pmap.DrawLine(gc, (sz / 4), (sz / 4), ((sz - 1) - (sz / 4)), ((sz - 1) - (sz / 4)));
+                        pmap.DrawLine(gc, ((sz - 1) - (sz / 4)), (sz / 4), (sz / 4), ((sz - 1) - (sz / 4)));
+                        return Gdk.Pixbuf.FromDrawable(pmap, pmap.Colormap, 0, 0, 0, 0, sz, sz);
+                    }
                 }
             }
         }
