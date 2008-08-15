fb34ede25600535ab027b5382ac460a84e34f037
longomatch <longomatch@f36f3c29-b420-0410-aad8-acd80f50a564>
2008-08-15 15:19:59 +0000

    Video

---
+++ LongoMatch/Video/FFMPEGVideoEditor.cs
@@ -0,0 +1,68 @@
+// FFMPEGVideoEditor.cs
+//
+//  Copyright (C) 2008 Andoni Morales Alastruey
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//
+
+using System;
+
+namespace LongoMatch
+{
+	
+	
+	public class FFMPEGVideoEditor : IVideoEditor
+	{
+		PlayList playlist;
+		string outputFile;
+		VideoQuality vq;
+		AudioQuality aq;
+		
+		public FFMPEGVideoEditor(PlayList playlist, string outputFile, VideoQuality vq, AudioQuality aq)
+		{
+			this.playlist = playlist;
+			this.outputFile = outputFile;
+			this.aq = aq;
+			this.vq = vq;
+			
+			
+		}
+		
+		PlayList PlayList {
+			set;
+		}
+		
+		VideoQuality VideoQuality{
+			set;
+		}
+		
+		AudioQuality AudioQuality{
+			set;
+		}
+		
+		string OutputFile{
+			set;
+		}
+		
+		bool EnableAudio{
+			set;
+		}
+		
+		void Start();
+		
+		void Cancel();
+	}
+}
