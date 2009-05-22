5b2efa05c660ca48b1d9eaaef9368fd29f5f6810
longomatch <longomatch@f36f3c29-b420-0410-aad8-acd80f50a564>
2009-05-22 09:39:17 +0000

    New widget for HotKey selection

---
+++ LongoMatch/Gui/HotKeySelectorDialog.cs
@@ -0,0 +1,49 @@
+
+using System;
+using Gtk;
+using Gdk;
+using LongoMatch.TimeNodes;
+
+namespace LongoMatch.Gui.Dialog
+{
+	
+	
+	public partial class HotKeySelectorDialog : Gtk.Dialog
+	{
+		HotKey hotKey;
+		
+#region Constructors
+		
+		public HotKeySelectorDialog()
+		{
+			hotKey = new HotKey();
+			this.Build();
+		}
+#endregion
+		
+#region Properties
+		
+		HotKey PressedHotKey{
+			get{return this.hotKey;}
+		}		
+#endregion
+		
+#region Overrides
+		
+		protected override bool OnKeyPressEvent (Gdk.EventKey evnt)
+		{
+			Gdk.Key key = evnt.Key;
+			ModifierType modifier = evnt.State;
+			
+			if ((modifier & (ModifierType.ControlMask | ModifierType.ShiftMask | ModifierType.SuperMask)) != 0){
+				hotKey.Key = key;
+				hotKey.Modifier = modifier;
+				this.Respond (ResponseType.Ok);
+			}
+			
+			return base.OnKeyPressEvent (evnt);
+		}
+#endregion
+
+	}
+}
