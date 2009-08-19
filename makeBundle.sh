bed6e20b0c500224f38eafb20e26f5f405ed42c9
Andoni Morales Alastruey <ylatuya@gmail.com>
2009-08-19 13:28:46 +0200

    Path to icon .rc file has changed

--- makeBundle.sh
+++ makeBundle.sh
@@ -3,6 +3,6 @@ $MONO_INSTALL_PATH/bin/gacutil -i LongoMatch/bin/Release/CesarPlayer.dll
 $MONO_INSTALL_PATH/bin/gacutil -i LongoMatch/bin/Release/Db4objects.Db4o.dll
 export PKG_CONFIG_PATH=$MONO_INSTALL_PATH/lib/pkgconfig/
 mkdir -p obj
-windres LongoMatch/minilogo.rc obj/minilogo.o
+windres LongoMatch/images/minilogo.rc obj/minilogo.o
 $MONO_INSTALL_PATH/bin/mono.exe $MONO_INSTALL_PATH/lib/mono/2.0/mkbundle.exe LongoMatch/bin/Release/LongoMatch.exe --deps -c -o obj/temp.c -oo obj/temp.o
 gcc -mno-cygwin -g -o obj/LongoMatch.exe -Wall obj/temp.c `pkg-config --cflags --libs mono|dos2unix`  obj/minilogo.o obj/temp.o 
