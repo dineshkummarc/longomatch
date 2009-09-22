e80c1a5cb3aea9e3b903ccc620db6dc1b963eeff
Andoni Morales Alastruey <ylatuya@gmail.com>
2009-09-22 14:26:20 +0200

    Delete unused files

--- LongoMatch/make.sh
+++
@@ -1,5 +0,0 @@
-/c/Archivos\ de\ programa/Mono-2.0/bin/gacutil -i bin/CesarPlayer.dll
-/c/Archivos\ de\ programa/Mono-2.0/bin/gacutil -i bin/Db4objects.Db4o.dll
-export PKG_CONFIG_PATH=/e/mono/lib/pkgconfig/
-/c/Archivos\ de\ programa/Mono-2.0/bin/mkbundle2 /LongoMatch.exe --deps -o win32/bin/LongoMatch.exe
-
