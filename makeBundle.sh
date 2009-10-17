4649dd32691d14eafa822e21112ac9b6365f097c
Andoni Morales Alastruey <ylatuya@gmail.com>
2009-10-17 23:25:22 +0200

    Improve win32 makefiles

--- makeBundle.sh
+++
@@ -1,8 +0,0 @@
-export MONO_INSTALL_PATH=/c/Mono-2.4/
-$MONO_INSTALL_PATH/bin/gacutil -i LongoMatch/bin/Release/CesarPlayer.dll
-$MONO_INSTALL_PATH/bin/gacutil -i LongoMatch/bin/Release/Db4objects.Db4o.dll
-export PKG_CONFIG_PATH=$MONO_INSTALL_PATH/lib/pkgconfig/
-mkdir -p obj
-windres LongoMatch/images/minilogo.rc obj/minilogo.o
-$MONO_INSTALL_PATH/bin/mono.exe $MONO_INSTALL_PATH/lib/mono/2.0/mkbundle.exe LongoMatch/bin/Release/LongoMatch.exe --deps -c -o obj/temp.c -oo obj/temp.o
-gcc -mno-cygwin -g -o obj/LongoMatch.exe -Wall obj/temp.c `pkg-config --cflags --libs mono|dos2unix`  obj/minilogo.o obj/temp.o 
