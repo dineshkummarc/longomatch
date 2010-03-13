354a8c7dcfc596c2f892635fa1120a4b11e6f758
Andoni Morales Alastruey <ylatuya@gmail.com>
2010-03-13 13:21:44 +0100

    Added new dialog to prompt when closing a capture project

--- LongoMatch/Gui/Dialog/EndCaptureDialog.cs
+++ LongoMatch/Gui/Dialog/EndCaptureDialog.cs
@@ -1,38 +1,44 @@
-//
-//  Copyright (C) 2009 Andoni Morales Alastruey
-//
+// 
+//  Copyright (C) 2010 Andoni Morales Alastruey
+// 
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
 //  (at your option) any later version.
-//
+// 
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//
+//  
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-//
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+// 
 
 using System;
+using LongoMatch.Common;
 
-namespace LongoMatch.Common
+namespace LongoMatch.Gui.Dialog
 {
-
-
-	public enum ProjectType {
-		NewCaptureProject,
-		NewFakeCaptureProject,
-		NewFileProject,
-		EditProject,
-		None,
-	}
 	
-	public enum EndCaptureResponse {
-		Return = 234,
-		Quit = 235,
-		Save = 236
+	
+	public partial class EndCaptureDialog : Gtk.Dialog
+	{
+		
+		public EndCaptureDialog()
+		{
+			this.Build();
+		}
+
+		protected virtual void OnQuit (object sender, System.EventArgs e)
+		{
+			if (sender == quitbutton)
+				Respond((int)EndCaptureResponse.Quit);
+			else if (sender == savebutton)
+				Respond((int)EndCaptureResponse.Save);
+			else
+				Respond((int)EndCaptureResponse.Return);
+		}
 	}
 }
