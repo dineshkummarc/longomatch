da8451a14c32f66f5b5e4ea43398e773a55d1bb6
Andoni Morales Alastruey <ylatuya@gmail.com>
2010-10-08 17:47:09 +0200

    Add brthday, height and weight properties to Players

--- LongoMatch/Time/Player.cs
+++ LongoMatch/Time/Player.cs
@@ -31,12 +31,25 @@ namespace LongoMatch.TimeNodes
 		private string position;
 		private int number;
 		private byte[] photo;
+		/* Added in 0.16.1 */
+		private float height;
+		private int weight;
+		private DateTime birthday;
 
 		/// <summary>
 		/// Creates a new player
 		/// </summary>
 		/// <param name="name">
 		/// A <see cref="System.String"/> with my name
+		/// </param>
+		/// <param name="birthday">
+		/// A <see cref="DateTime"/> with my name
+		/// </param>
+		/// <param name="height">
+		/// A <see cref="System.Float"/> with my height
+		/// </param>
+		/// <param name="weight">
+		/// A <see cref="System.Int32"/> with my weight
 		/// </param>
 		/// <param name="position">
 		/// A <see cref="System.String"/> with my position in the field
@@ -48,9 +61,13 @@ namespace LongoMatch.TimeNodes
 		/// A <see cref="Pixbuf"/> with my photo
 		/// </param>
 		#region Constructors
-		public Player(string name, string position, int number, Pixbuf photo)
+		public Player(string name, DateTime birthday, float height, int weight,
+		              string position, int number, Pixbuf photo)
 		{
 			this.name = name;
+			this.birthday = birthday;
+			this.height = height;
+			this.weight = weight;
 			this.position = position;
 			this.number = number;
 			Photo = photo;
@@ -109,6 +126,42 @@ namespace LongoMatch.TimeNodes
 					photo=value.SaveToBuffer("png");
 				else
 					photo=null;
+			}
+		}
+		
+		/// <value>
+		/// My birthdayt
+		/// </value>
+		public DateTime Birthday{
+			get {
+				return birthday;
+			}
+			set {
+				birthday = value;
+			}
+		}
+		
+		/// <value>
+		/// My height
+		/// </value>
+		public float Height{
+			get {
+				return height;
+			}
+			set {
+				height = value;
+			}
+		}
+
+		/// <value>
+		/// My Weight
+		/// </value>
+		public int Weight{
+			get {
+				return weight;
+			}
+			set {
+				weight = value;
 			}
 		}
 		#endregion
